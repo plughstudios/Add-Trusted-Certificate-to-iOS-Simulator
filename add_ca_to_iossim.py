acb054b2b5f2c0a47a9478b869230c179145cf5b
mwielgoszewski <marcin.wielgoszewski@gmail.com>
2013-04-02 09:15:14 -0400

    expand tabs as 4 spaces

--- add_ca_to_iossim.py
+++ add_ca_to_iossim.py
@@ -1,5 +1,5 @@
+# -*- coding: utf-8 -*-
 '''
-
     add_ca_to_iossim.py v0.1
     Copyright (C) 2011 Ron Gutierrez
 
@@ -17,11 +17,9 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 '''
-
-import sqlite3
 import os
+import sqlite3
 import subprocess
-from optparse import OptionParser
 
 __usage__ = """
 Please supply required arguments: <CA Certificate Path>
@@ -34,56 +32,57 @@ truststore_path = "/Library/Keychains/TrustStore.sqlite3"
 
 
 def cert_fingerprint_via_openssl(cert_location):
-	output = subprocess.check_output(["openssl", "x509", "-noout", "-in", cert_location, "-fingerprint"])
-	fingerprint_with_colons = output.split("=")[1]
-	return fingerprint_with_colons.replace(':','')
+    output = subprocess.check_output(["openssl", "x509", "-noout", "-in", cert_location, "-fingerprint"])
+    fingerprint_with_colons = output.split("=")[1]
+    return fingerprint_with_colons.replace(':','')
 
 
 def cert_fingerprint(cert_location):
-	try:
-		from M2Crypto import X509	
-		cert = X509.load_cert(cert_location)
-		return cert.get_fingerprint('sha1')
-	except ImportError:
-		return cert_fingerprint_via_openssl(cert_location)	
+    try:
+        from M2Crypto import X509   
+        cert = X509.load_cert(cert_location)
+        return cert.get_fingerprint('sha1')
+    except ImportError:
+        return cert_fingerprint_via_openssl(cert_location)  
 
 
 def add_to_truststore(sdk_dir, cert_fingerprint):
-	tpath = simulator_dir+sdk_dir+truststore_path
+    tpath = simulator_dir+sdk_dir+truststore_path
 
-	sha1="X'"+cert_fingerprint.strip()+"'"
+    sha1="X'"+cert_fingerprint.strip()+"'"
 
-	try:
-		conn = sqlite3.connect(simulator_dir+sdk_dir+truststore_path)
-		c = conn.cursor()
-		sql = 'insert into tsettings values (%s,%s,%s,%s)'%(sha1, "randomblob(16)", "randomblob(16)", "randomblob(16)")
-		c.execute(sql)
-		conn.commit()
+    try:
+        conn = sqlite3.connect(simulator_dir+sdk_dir+truststore_path)
+        c = conn.cursor()
+        sql = 'insert into tsettings values (%s,%s,%s,%s)'%(sha1, "randomblob(16)", "randomblob(16)", "randomblob(16)")
+        c.execute(sql)
+        conn.commit()
 
-		c.close()
-		conn.close()
-		print("Successfully added CA to %s" % tpath) 
-	except sqlite3.OperationalError:
-		print("Error adding CA to %s" % tpath )
-		print("Mostly likely failed because Truststore does not exist..skipping\n")
-		return
-	except sqlite3.IntegrityError:
-		print("Error adding CA to %s" % tpath )
-		print("Table already has an entry with the same CA SHA1 fingerprint..skipping\n")
-		return
+        c.close()
+        conn.close()
+        print("Successfully added CA to %s" % tpath) 
+    except sqlite3.OperationalError:
+        print("Error adding CA to %s" % tpath )
+        print("Mostly likely failed because Truststore does not exist..skipping\n")
+        return
+    except sqlite3.IntegrityError:
+        print("Error adding CA to %s" % tpath )
+        print("Table already has an entry with the same CA SHA1 fingerprint..skipping\n")
+        return
 
 if __name__ == "__main__":
-	parser = OptionParser(usage=__usage__)
-	opt, args = parser.parse_args()
+    from optparse import OptionParser
+    parser = OptionParser(usage=__usage__)
+    opt, args = parser.parse_args()
 
-	if len(args) < 1:
-		parser.print_help()
-		exit(1)
+    if len(args) < 1:
+        parser.print_help()
+        exit(1)
 
-	cert_location = args[0]
+    cert_location = args[0]
 
-	cert_fingerprint = cert_fingerprint(cert_location)
+    cert_fingerprint = cert_fingerprint(cert_location)
 
-	for sdk_dir in os.listdir(simulator_dir):
-		if not sdk_dir.startswith('.') and sdk_dir != 'User':
-			add_to_truststore(sdk_dir, cert_fingerprint)
+    for sdk_dir in os.listdir(simulator_dir):
+        if not sdk_dir.startswith('.') and sdk_dir != 'User':
+            add_to_truststore(sdk_dir, cert_fingerprint)
